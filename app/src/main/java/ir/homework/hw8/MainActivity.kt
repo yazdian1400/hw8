d016aed45eeaf782381fa050bb56b1de28e92214
Maedeh <yazdian.oul@gmail.com>
2022-03-03 23:22:49 +0330

    set onClickListeners for register button

--- app/src/main/java/ir/homework/hw8/MainActivity.kt
+++ app/src/main/java/ir/homework/hw8/MainActivity.kt
@@ -2,14 +2,33 @@ package ir.homework.hw8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import ir.homework.hw8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        onClickListeners()
+    }
+
+    private fun onClickListeners() {
+        binding.btnSubmit.setOnClickListener {
+            if (binding.etName.text.isNullOrBlank())
+                binding.etName.error = "لطفا نام و نام خانوادگی را وارد کنید"
+            if (binding.etNationalCode.text.isNullOrBlank())
+                binding.etNationalCode.error = "لطفا کد ملی خود را وارد کنید"
+            else if (binding.etNationalCode.text.toString().trim().length != 10)
+                binding.etNationalCode.error = "کد ملی وارد شده معتبر نیست"
+            if (binding.etPlaceBirth.text.isNullOrBlank())
+                binding.etPlaceBirth.error = "لطفا محل تولد خود را وارد کنید"
+            if (binding.etAddress.text.isNullOrBlank())
+                binding.etAddress.error = "لطفا آدرس خود را وارد کنید"
+            if (binding.etZipCode.text.isNullOrBlank())
+                binding.etZipCode.error = "لطفا کد پستی خود را وارد کنید"
+        }
     }
 }
